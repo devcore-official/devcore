ece6a02be217a7e26aa09dfeb1545c8ad863e801
neur0plasticity <djtinkers365@gmail.com>
2021-10-08 13:40:09 -0700

    datatypes cpp filled out

--- rosetta/datatypes.js
+++ rosetta/datatypes.js
@@ -1,5 +1,4 @@
 module.exports = [
-
     /**
      * 
      * it seems the popular general programming 
@@ -16,38 +15,7 @@ module.exports = [
      * offering 8 bit, 16 bit, 32bit, and 64bit 
      * ints, floats, and chars
      *  
-     * 
-     * 
-     * 
      */
-{
-
-    name: "",
-
-
-    javascript: "",
-    nodejs:     "",
-    python:     "",
-    php:        "",
-    ruby:       "",
-    perl:       "",
-    lua:        "",
-    r:          "",
-    lisp:       "",
-    kotlin:     "",
-    swift:      "",
-    go:         "",
-    java:       "",
-    cpp:        "",
-    c:          "",
-    "c#":       "",
-    rust:       "",
-    fortran:    "",
-    x86:        "",
-    arm32:      "",
-    arm64:      "",
-    wasm:       ""
-},
 {
     name: "string",
 
@@ -127,5 +95,98 @@ module.exports = [
     arm64:      "",
     wasm:       ""
 },
+{
+
+    name: "",
+
+
+//    bits: 
+
+
+    javascript: "",
+    nodejs:     "",
+    python:     "",
+    php:        "",
+    ruby:       "",
+    perl:       "",
+    lua:        "",
+    r:          "",
+    lisp:       "",
+    kotlin:     "",
+    swift:      "",
+    go:         "",
+    java:       "",
+    cpp:        "",
+    c:          
+    "c#":       "",
+    rust:       "",
+    fortran:    "",
+    x86:        "",
+    arm32:      "",
+    arm64:      "",
+    wasm:       ""
+},
+
+c = [
+	// type	      		bits	val range
+
+integers: [
+	["char",		8,	[-128,127]],
+	["unsigned char",	8,	[0,255]],
+	["signed char",		8,	[-128,127]],
+	["int",			[16,32],[[-32768,32767],[-2147483648, 2147483647]]],
+	["unsigned int",	[16,32],[[0,65535],[0,4294967295]]],
+	["short"		16,	[-32768,32767]],
+	["unsigned short",	16,	[0,65535]],
+	["long",		64, 	[-9223372036854775808,9223372036854775807]],
+	["unsigned long",	64,	[0,18446744073709551615]]
+],
+
+floats:[
+	// type		bits	value range		decimal precision
+	["float",	32, 	[1.2E-38, 3.4E+38],	6],
+	["double"	64,	[2.3E-308, 1.7E+308],	15],
+	["long double",	80,	[3.4E-4932, 1.1E+4932],	19]
+],
+
+enums:[
+	// 
+],
+
+derived:[
+
+],
+
+
+]
+
+
+cpp:[
+
+	// type			bits	value range
+	["char",	 	8,	[[-127 to 127], [0 to 255]]],
+	["unsigned char",	8,	[[0,255]]],
+	["signed char",		8,	[[-127,127]]],
+	["int",			32,	[[-2147483648,2147483647]]],
+	["unsigned int",	32,	[[0 to 4294967295]]],
+	["signed int",		32,	[[-2147483648,2147483647]]],
+	["short int",		16,	[[-32768,32767]]],
+	["unsigned short int",	16,	[[0, 65535]]],
+	["signed short int",	16,	[[-32768, 32767]]],
+	["long int",		64,	[[-2147483648, 2147483647]]],
+	["signed long int",	64,	[[-2147483648, 2147483647]]],
+	["unsigned long int",	64,	[[0, 4294967295]]],
+	["long long int",	64,	[[-(2**63), (2**63)-1]]],
+	["unsigned long long int",64,	[[0,18446744073709551615]]],
+	["float",		32,	[[]]],	
+	["double",		64,	[[]]],
+	["long double",		96,	[[]]],
+	["wchar_t",		[16,32],[[]]],
+]
+
+
+
+
+
 
 ]
