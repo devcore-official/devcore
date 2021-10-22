0079fd13e0f4cb55d8e4242a0d549acf541e962c
neur0plasticity <you@example.com>
2021-10-22 12:32:06 -0700

    devcore service repos added

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
 
 const express = require("express");
 const app = express();
+const execSync = require("child_process").execSync;
 ////////////////////////
 
 const defaultPort = 8080;
@@ -17,21 +18,53 @@ let apis = [
 
 console.log("currently turns all dependencies into local apis");
 let deps = require("./package.json").dependencies
+		
+// Object.keys(deps)
+// 	.filter((dep)=>{ return dep !== "express" })
+// 	.forEach(
+// 		(dep)=>{
+// 			console.log(`execSync ("git clone https://github.com/devcore-official/${ dep }.git")`);
+// 		}
+// 	);
+
+execSync ("git clone https://github.com/devcore-official/DStructs-Wikipedia.git")
+execSync ("git clone https://github.com/devcore-official/DStruct-assembler.git")
+execSync ("git clone https://github.com/devcore-official/Dstruct-intuit.git")
+execSync ("git clone https://github.com/devcore-official/algosynthesis.git")
+execSync ("git clone https://github.com/devcore-official/always-building.git")
+execSync ("git clone https://github.com/devcore-official/always-optimizing.git")
+execSync ("git clone https://github.com/devcore-official/always-solving.git")
+execSync ("git clone https://github.com/devcore-official/always-testing.git")
+execSync ("git clone https://github.com/devcore-official/auto-app-maker.git")
+execSync ("git clone https://github.com/devcore-official/datastructures.git")
+execSync ("git clone https://github.com/devcore-official/everything-api.git")
+execSync ("git clone https://github.com/devcore-official/interface.git")
+execSync ("git clone https://github.com/devcore-official/memory.git")
+execSync ("git clone https://github.com/devcore-official/ml-code-optimization.git")
+execSync ("git clone https://github.com/devcore-official/niche-finder.git")
+execSync ("git clone https://github.com/devcore-official/piworker.git")
+execSync ("git clone https://github.com/devcore-official/polyglot.git")
+execSync ("git clone https://github.com/devcore-official/project-management.git")
+execSync ("git clone https://github.com/devcore-official/registry.git")
+execSync ("git clone https://github.com/devcore-official/schematics.git")
+execSync ("git clone https://github.com/devcore-official/spec-dev.git")
+execSync ("git clone https://github.com/devcore-official/stay-alive.git")
 
 Object.keys(deps).filter((dep)=>{ return dep !== "express" }).forEach(
 	(dep,index)=>{
 
-		apis.push(
-			{	
-				port 		:(defaultPort + index), 
-				servicename	:(deps),
-				url			:`localhost:${defaultPort + index}`,
-			    serviceCB	:require(dep),
-			    app         :app
-			}
-		)
 
-		app.listen(apis[index].port);
+		// apis.push(
+		// 	{	
+		// 		port 		:(defaultPort + index), 
+		// 		servicename	:(deps),
+		// 		url			:`localhost:${defaultPort + index}`,
+		// 	    serviceCB	:require(dep),
+		// 	    app         :app
+		// 	}
+		// )
+
+		// app.listen(apis[index].port);
 		// pingServicesEverySecond:
 		// setInterval(()=>{
 		// 	https.request(apis[index].url).then()
