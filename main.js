d699d72c340e2c9c5a7044bee36c079deb99c081
neur0plasticity <you@example.com>
2021-10-22 19:06:00 -0700

    altered always repositories to one

--- main.js
+++ main.js
@@ -23,51 +23,55 @@ let deps = require("./package.json").dependencies
 // 	.filter((dep)=>{ return dep !== "express" })
 // 	.forEach(
 // 		(dep)=>{
-// 			console.log(`execSync ("git clone https://github.com/devcore-official/${ dep }.git")`);
+// 			execSync(`cd ./node_modules && git clone https://github.com/devcore-official/${ dep }.git`);
 // 		}
 // 	);
 
-execSync ("git clone https://github.com/devcore-official/DStructs-Wikipedia.git")
-execSync ("git clone https://github.com/devcore-official/DStruct-assembler.git")
-execSync ("git clone https://github.com/devcore-official/Dstruct-intuit.git")
-execSync ("git clone https://github.com/devcore-official/algosynthesis.git")
-execSync ("git clone https://github.com/devcore-official/always-building.git")
-execSync ("git clone https://github.com/devcore-official/always-optimizing.git")
-execSync ("git clone https://github.com/devcore-official/always-solving.git")
-execSync ("git clone https://github.com/devcore-official/always-testing.git")
-execSync ("git clone https://github.com/devcore-official/auto-app-maker.git")
-execSync ("git clone https://github.com/devcore-official/datastructures.git")
-execSync ("git clone https://github.com/devcore-official/everything-api.git")
-execSync ("git clone https://github.com/devcore-official/interface.git")
-execSync ("git clone https://github.com/devcore-official/memory.git")
-execSync ("git clone https://github.com/devcore-official/ml-code-optimization.git")
-execSync ("git clone https://github.com/devcore-official/niche-finder.git")
-execSync ("git clone https://github.com/devcore-official/piworker.git")
-execSync ("git clone https://github.com/devcore-official/polyglot.git")
-execSync ("git clone https://github.com/devcore-official/project-management.git")
-execSync ("git clone https://github.com/devcore-official/registry.git")
-execSync ("git clone https://github.com/devcore-official/schematics.git")
-execSync ("git clone https://github.com/devcore-official/spec-dev.git")
-execSync ("git clone https://github.com/devcore-official/stay-alive.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/DStructs-Wikipedia.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/DStruct-assembler.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/Dstruct-intuit.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/algosynthesis.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/always-building.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/always-optimizing.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/always-solving.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/always-testing.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/auto-app-maker.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/datastructures.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/everything-api.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/interface.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/memory.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/ml-code-optimization.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/niche-finder.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/piworker.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/polyglot.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/project-management.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/registry.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/schematics.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/spec-dev.git")
+// execSync ("cd ./node_modules && git clone https://github.com/devcore-official/stay-alive.git")
+
+// execSync("npm install");
+// execSync("npm install");
+
 
 Object.keys(deps).filter((dep)=>{ return dep !== "express" }).forEach(
 	(dep,index)=>{
 
 
-		// apis.push(
-		// 	{	
-		// 		port 		:(defaultPort + index), 
-		// 		servicename	:(deps),
-		// 		url			:`localhost:${defaultPort + index}`,
-		// 	    serviceCB	:require(dep),
-		// 	    app         :app
-		// 	}
-		// )
+		apis.push(
+			{	
+				port 		:(defaultPort + index), 
+				servicename	:(deps),
+				url			:`localhost:${defaultPort + index}`,
+			    serviceCB	:require(dep),
+			    app         :app
+			}
+		)
 
-		// app.listen(apis[index].port);
-		// pingServicesEverySecond:
-		// setInterval(()=>{
-		// 	https.request(apis[index].url).then()
-		// },1000);
+		app.listen(apis[index].port);
+		pingServicesEverySecond:
+		setInterval(()=>{
+			https.request(apis[index].url).then()
+		},1000);
 	}
 );
